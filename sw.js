114e37e6a31e32bec849d11b04049b5baa5f5365
gaby975 <benvenistegabriel@gmail.com>
2025-12-11 15:50:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 // sw.js
-const CACHE_NAME = 'app-cache-v8';
+const CACHE_NAME = 'app-cache-v9';
 
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json?v=8',
-  './icons/icon-192.png?v=8',
-  './icons/icon-512.png?v=8'
+  './manifest.json?v=9',
+  './icons/icon-192.png?v=9',
+  './icons/icon-512.png?v=9'
 ];
 
 self.addEventListener('install', event => {
@@ -44,6 +44,7 @@ self.addEventListener('fetch', event => {
     caches.match(req, { ignoreSearch: true }).then(resp => resp || fetch(req))
   );
 });
+
 
 
 
